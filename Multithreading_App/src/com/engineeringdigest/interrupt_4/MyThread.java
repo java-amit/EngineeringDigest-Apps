47074b5d44893e12bae2eee65bab6af82304a999
amit <amitbh94@gmail.com>
2025-11-04 10:37:04 +0530

    interrupt to thread t1

---
+++ Multithreading_App/src/com/engineeringdigest/interrupt_4/MyThread.java
@@ -0,0 +1,23 @@
+package com.engineeringdigest.interrupt_4;
+
+public class MyThread extends Thread{
+	
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(1000);
+			System.out.println("Thread is running");
+		} catch (InterruptedException e) {
+			System.out.println("Thread interrupted: "+e);
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		MyThread t1 = new MyThread();
+		t1.start();
+		t1.interrupt();
+		
+	}
+
+}
